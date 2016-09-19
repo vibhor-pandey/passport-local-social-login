fae7a91267024c22e87018272b86fe30d30ca475
Vibhor Pandey <vibhor.vibhu@gmail.com>
2016-09-19 15:35:26 +0530

    Update user.js

--- app/models/user.js
+++ app/models/user.js
@@ -9,11 +9,6 @@ var userSchema = mongoose.Schema({
 		verified: Boolean
 	},
 
-	localVerify:{
-		userId: String,
-		verificationToken: String
-	},
-
 	facebook: {
 		id: String,
 		token: String,
@@ -50,12 +45,12 @@ var userSchema = mongoose.Schema({
 });
 
 userSchema.methods.generateHash = function(password) {
-	console.log("generateHash");
+	//console.log("generateHash");
 	return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
 };
 
 userSchema.methods.validPassword = function(password) {
-	console.log("compareHash");
+	//console.log("compareHash");
 	return bcrypt.compareSync(password, this.local.password);
 };
 
