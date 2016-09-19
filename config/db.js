cd011e72423d1424c8a0b158637cd4e39014033f
Vibhor Pandey <vibhor.vibhu@gmail.com>
2016-09-19 15:39:50 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -6,6 +6,6 @@ var mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 //mongoose.connect('mongodb://10.7.0.3:27107/data/db');
 
-mongoose.connect('mongodb://auth:auth@ds019956.mlab.com:19956/authenticate');
+mongoose.connect('mongodb://<mlab_bd_name>:<mlab_db_password>@ds019956.mlab.com:19956/authenticate');
 console.log("db.js started...");
 module.exports = mongoose.connection;
